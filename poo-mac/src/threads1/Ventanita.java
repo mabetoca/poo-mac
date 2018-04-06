12bea098a1ab355c7d4f98831c11f3b0c7052da2
T-102 <T-102@PC250>
2018-04-05 19:07:17 -0500

    Solucion

--- poo-mac/src/threads1/Ventanita.java
+++ poo-mac/src/threads1/Ventanita.java
@@ -6,37 +6,34 @@
 package threads1;
 
 import java.time.LocalTime;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author T-102
  */
-public class Relojito extends javax.swing.JFrame {
+public class Ventanita extends javax.swing.JFrame {
 
     /**
-     * Creates new form Relojito
+     * Creates new form Ventanita
      */
-   
-                
-    public Relojito() {
+    public Ventanita() {
         initComponents();
-        setAlwaysOnTop(true);
+        setSize(500,500);
         
         Thread t1=new Thread(new Runnable(){
             @Override
             public void run() {
                 
+                int decremento=0;
+                        
                 while(true){
                     
                     try {
+                        decremento=decremento - 10;
                         
-                        LocalTime tiempo=LocalTime.now();
-                         int hora=tiempo.getHour();
-                         int minutos=tiempo.getMinute();
-                         int segundos=tiempo.getSecond();
-                         jLabel1.setText(hora+":"+minutos+":"+segundos);
+                        //if(500-decremento < 0)break;
+                        setSize(500+decremento,500+decremento);
+                                                                
             
                          //PONEMOS A DORMIR AL THREAD
                          Thread.sleep(1000);
@@ -51,8 +48,6 @@ public class Relojito extends javax.swing.JFrame {
         
         );
         t1.start();
-        
-      
     }
 
     /**
@@ -64,32 +59,17 @@ public class Relojito extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 11, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 65, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -99,8 +79,6 @@ public class Relojito extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        
-       
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -114,33 +92,24 @@ public class Relojito extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Relojito.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ventanita.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Relojito().setVisible(true);
-                
-                            
-               
+                new Ventanita().setVisible(true);
             }
         });
     }
-    
-   
 
-    
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private static javax.swing.JLabel jLabel1;
-    private static javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
